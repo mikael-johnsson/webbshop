a04780418de8b8acb1b56aa2916e282a23f96318
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-01-07 09:11:01 +0100

    add serviceBase

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
 import "./style.css";
 import "./style.scss";
+import "./services/serviceBase";
+import { getProducts } from "./services/serviceBase";
+
+const data = await getProducts();
+console.log(data.products);
