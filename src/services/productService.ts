ecc4206c3b0b674f2beea39c7c2e15bd8ee09abd
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-01-07 17:48:30 +0100

    add a getProducts function

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -3,11 +3,13 @@ import { getData } from "./serviceBase";
 /**
  * This function can be used to filter the fetch
  */
+export const getProducts = async () => {
+  const productResponse = await getData();
+  return productResponse.products;
+};
+
 export const getProductsById = async (id: string) => {
   const productResponse = await getData();
   const product = productResponse.products.find((p) => p.id === parseFloat(id));
   return product;
 };
-
-const product = await getProductsById("1");
-// console.log("this is product with id 1:", product);
