cca97f0b69258440ca4121dd042068e35b8e0a24
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-01-07 09:44:42 +0100

    add getProducts function to later use to filter the data fetch

--- src/services/serviceBase.ts
+++ src/services/serviceBase.ts
@@ -5,7 +5,7 @@ import type { ProductResponse } from "../models/ProductResponse";
  * @returns A productResponse object. It only consists of one attribute,
  * (products), which is an array of Product.
  */
-export const getProducts = async () => {
+export const getData = async () => {
   const response = await fetch("/products.json");
   const data: ProductResponse = await response.json();
   return data;
