8c9053d4b29a7d60943a18dea24249529d735b90
Liudmyla Valier <166840501+Valersha88888888@users.noreply.github.com>
2026-01-07 12:00:52 +0100

    landningssida  hero + CTA + SCSS

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
-import "./style.css";
 import "./style.scss";
+console.log("MAIN LOADED");
+
+const app = document.querySelector<HTMLDivElement>("#app");
+if (!app) throw new Error("Missing #app element");
+
+startRouter(app);
+app.innerHTML = "<h1>APP FUNKAR</h1>";
+
+
