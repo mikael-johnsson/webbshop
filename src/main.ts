aec6b9f709d1853bc699dbe106513fd8bb5b11dd
Lo <lovisaornestal@gmail.com>
2026-01-08 14:19:46 +0100

    test on main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,12 @@ import "./style.css";
 import "./style.scss";
 import "./services/productService";
 import "./utils/cartUtils";
+import { initPdp } from "./pages/pdp/pdp";
+
+
+
+// Kör PDP-kod bara på pdp-sidan
+if (window.location.pathname.includes("pdp")) {
+  console.log("CALLING initPdp");
+  initPdp();
+}
