c55c7f8616b183269a17a3bbe8975269f0d5a550
Lo <lovisaornestal@gmail.com>
2026-01-19 20:30:22 +0100

    fixed styling, synced hamburgermenu + cartPop

--- src/utils/cartModalUtils.ts
+++ src/utils/cartModalUtils.ts
@@ -1,4 +1,5 @@
 import type { Cart } from "../models/Cart";
+
 console.log("cartPop.ts loaded");
 
 const getCartFromLS = (): Cart | null => {
@@ -9,7 +10,9 @@ const getCartFromLS = (): Cart | null => {
 
 
 export const renderCartPop = (cartPop: HTMLElement) => {
-    const cart = getCartFromLS() as any;
+    const cart = getCartFromLS();
+    if(!cart) return
+
     const items = cart?.items ?? [];
 
     if(!cartPop) return;
@@ -21,13 +24,27 @@ export const renderCartPop = (cartPop: HTMLElement) => {
     }
 
     items.forEach((item) => {
+      const row = document.createElement("div");
+      row.className ="cartRow"
 
+      const img = document.createElement("img");
+      img.className ="cartRow__img";
+      img.src = item.product.image;
      
-        const p = document.createElement("p");
-        p.style.padding ="10px 20px";
-        p.textContent = `${item.product.name} x ${item.amount}`
+        const nameAmount = document.createElement("p");
+        nameAmount.className ="cartRow__nameAmount";
+        nameAmount.textContent = `${item.product.name} x ${item.amount}`
 
-        cartPop.appendChild(p);
+       const total = item.product.price * item.amount;
+      const totalPrice = document.createElement("p");
+      totalPrice.className = "cartRow__price"
+
+      totalPrice.textContent = `${total} SEK`
+
+        row.appendChild(img);
+        row.appendChild(nameAmount)
+        row.appendChild(totalPrice)
+        cartPop.appendChild(row)
     });
     console.log("LS cart raw:", localStorage.getItem("cart"));
     console.log("cart parsed:", getCartFromLS());
