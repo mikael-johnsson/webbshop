5c52b1b7b85def6146f8b8216b9f89af3f967545
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-01-29 10:22:29 +0100

    fix type in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         pdp: resolve(__dirname, "pdp.html"),
         checkout: resolve(__dirname, "checkout.html"),
-        cartPage: resolve(__dirname, "cartPage.html"),
+        cartPage: resolve(__dirname, "cartpage.html"),
         newDrops: resolve(__dirname, "newdrops.html"),
       },
     },
