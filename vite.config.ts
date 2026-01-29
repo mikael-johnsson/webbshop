3046156ad01fc05328bec31413ff1ba887336c9c
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-01-29 09:52:54 +0100

    vite configs for deployment

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        pdp: resolve(__dirname, "pdp.html"),
+        checkout: resolve(__dirname, "checkout.html"),
+        cartPage: resolve(__dirname, "cartPage.html"),
+        newDrops: resolve(__dirname, "newDrops.html"),
+      },
+    },
+  },
+});
